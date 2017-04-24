544f77cefcb1b32f163ab887e1b54020e96714b2
Jake Romer <jkrmr.io@gmail.com>
2017-04-24 14:20:36 -0700

    Add button with constraints

--- CoreDataHotel/Controllers/HotelViewController.h
+++ CoreDataHotel/Controllers/HotelViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "RoomsViewController.h"
+#import "AutoLayout.h"
 
 @interface HotelViewController : UIViewController
 @end
--- CoreDataHotel/Controllers/HotelViewController.m
+++ CoreDataHotel/Controllers/HotelViewController.m
@@ -14,6 +14,44 @@
 @implementation HotelViewController
 - (void)viewDidLoad {
   [super viewDidLoad];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
   self.view.backgroundColor = [UIColor whiteColor];
+  [self setupLayout];
+}
+
+- (void) setupLayout {
+  UIButton *browseButton = [self createButtonWithTitle:@"Browse"];
+  [AutoLayout leadingConstraintFromView:browseButton toView:self.view];
+  [AutoLayout trailingConstraintFromView:browseButton toView:self.view];
+
+  browseButton.backgroundColor = [UIColor colorWithRed:1 green:1 blue:0.75 alpha:1];
+  [AutoLayout equalHeightConstraintFromView:browseButton toView:self.view withMultiplier:0.33];
+  
+  [browseButton addTarget:self
+                   action:@selector(browseButtonWasPressed)
+         forControlEvents:UIControlEventTouchUpInside];
+
+//  UIButton *bookButton = [self createButtonWithTitle:@"Book"];
+//  [AutoLayout leadingConstraintFromView:bookButton toView:self.view];
+//  [AutoLayout trailingConstraintFromView:bookButton toView:self.view];
+//
+//  UIButton *lookupButton = [self createButtonWithTitle:@"Look up"];
+//  [AutoLayout leadingConstraintFromView:lookupButton toView:self.view];
+//  [AutoLayout trailingConstraintFromView:lookupButton toView:self.view];
+}
+
+- (void) browseButtonWasPressed {
+  NSLog(@"selected");
+}
+
+- (UIButton*) createButtonWithTitle:(NSString*)title {
+  UIButton *button = [[UIButton alloc] init];
+  [button setTitle:title forState:UIControlStateNormal];
+  [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+  [button setTranslatesAutoresizingMaskIntoConstraints:NO];
+  [self.view addSubview:button];
+  return button;
 }
 @end
