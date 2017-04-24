327ae4569001bb06266c885f63d9ce159e6b3a7b
Jake Romer <jkrmr.io@gmail.com>
2017-04-24 09:32:39 -0700

    Initial commit

---
+++ CoreDataHotel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataHotel
+//
+//  Created by Jake Romer on 4/24/17.
+//  Copyright © 2017 Jake Romer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
