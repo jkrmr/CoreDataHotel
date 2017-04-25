4849080ae0f153afcc8abf141e63e78fd43ca0a8
Jake Romer <jkrmr.io@gmail.com>
2017-04-25 09:31:52 -0700

    Add DatePickerVC with start and end date pickers

---
+++ CoreDataHotel/Controllers/DatePickerViewController.h
@@ -0,0 +1,12 @@
+//
+//  DatePickerViewController.h
+//  CoreDataHotel
+//
+//  Created by Jake Romer on 4/25/17.
+//  Copyright © 2017 Jake Romer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DatePickerViewController : UIViewController
+@end
---
+++ CoreDataHotel/Controllers/DatePickerViewController.m
@@ -0,0 +1,20 @@
+//
+//  DatePickerViewController.m
+//  CoreDataHotel
+//
+//  Created by Jake Romer on 4/25/17.
+//  Copyright © 2017 Jake Romer. All rights reserved.
+//
+
+#import "DatePickerViewController.h"
+
+@interface DatePickerViewController ()
+@property (strong, nonatomic) UIDatePicker *startDate;
+@property (strong, nonatomic) UIDatePicker *endDate;
+@end
+
+@implementation DatePickerViewController
+- (void)viewDidLoad {
+  [super viewDidLoad];
+}
+@end
