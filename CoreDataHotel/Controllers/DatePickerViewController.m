edd07e0cbe569148cc5fd729596d02bc29170ea1
Jake Romer <jkrmr.io@gmail.com>
2017-04-25 10:27:03 -0700

    Add start date date picker

--- CoreDataHotel/Controllers/DatePickerViewController.m
+++ CoreDataHotel/Controllers/DatePickerViewController.m
@@ -17,5 +17,16 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.view.backgroundColor = [UIColor whiteColor];
+
+  // set up start date picker
+  self.startDate = [[UIDatePicker alloc] init];
+  [self.view addSubview:self.startDate];
+  self.startDate.translatesAutoresizingMaskIntoConstraints = NO;
+  [[[self.startDate leadingAnchor] constraintEqualToAnchor:[self.view leadingAnchor]] setActive:YES];
+  [[[self.startDate trailingAnchor] constraintEqualToAnchor:[self.view trailingAnchor]] setActive:YES];
+  [[[self.startDate bottomAnchor] constraintEqualToAnchor:[self.view bottomAnchor]] setActive:YES];
+  [[[self.startDate heightAnchor] constraintEqualToAnchor:[self.view heightAnchor] multiplier:0.3] setActive:YES];
+
+  // set up end date picker
 }
 @end
