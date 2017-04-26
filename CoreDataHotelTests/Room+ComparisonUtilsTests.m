221b97034e8625ce9972c5efdb1c8ca996812ac9
Jake Romer <jkrmr.io@gmail.com>
2017-04-26 10:32:17 -0700

    Add test file for Room comparison utils

---
+++ CoreDataHotelTests/Room+ComparisonUtilsTests.m
@@ -0,0 +1,31 @@
+//
+//  Room+ComparisonUtilsTests.m
+//  Room+ComparisonUtilsTests
+//
+//  Created by Jake Romer on 4/24/17.
+//  Copyright © 2017 Jake Romer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Room_ComparisonUtilsTests : XCTestCase
+@end
+
+@implementation Room_ComparisonUtilsTests
+
+- (void)setUp {
+  [super setUp];
+}
+
+- (void)tearDown {
+  [super tearDown];
+}
+
+- (void)testCompareComparesByRoomNumber {
+}
+
+- (void)testPerformanceExample {
+  [self measureBlock:^{
+  }];
+}
+@end
