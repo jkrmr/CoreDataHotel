7158e596faed1b2506bc1e0a6842378fc41b9728
Jake Romer <jkrmr.io@gmail.com>
2017-04-26 11:57:29 -0700

    Add test file for Reservation class

---
+++ CoreDataHotelTests/ReservationTest.m
@@ -0,0 +1,20 @@
+//
+//  ReservationTest.m
+//  CoreDataHotel
+//
+//  Created by Jake Romer on 4/26/17.
+//  Copyright © 2017 Jake Romer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "XCCoreDataTestCase.h"
+#import "Reservation+CoreDataClass.h"
+#import "Reservation+CoreDataProperties.h"
+
+@interface ReservationTest : XCCoreDataTestCase
+@end
+
+@implementation ReservationTest
+- (void)testOverlappingReservationLogic {
+}
+@end
