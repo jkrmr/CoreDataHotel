8c0798a0221287baca0ce10201136125a934eb1f
Jake Romer <jkrmr.io@gmail.com>
2017-04-25 18:13:44 -0700

    Refactor BookVC text fields to use auto layout

--- CoreDataHotel/Controllers/BookViewController.m
+++ CoreDataHotel/Controllers/BookViewController.m
@@ -19,35 +19,49 @@
   [super viewDidLoad];
   self.view.backgroundColor = [UIColor whiteColor];
 
-  UITextField *firstName = [[UITextField alloc] initWithFrame:CGRectMake(45, 30, 200, 40)];
+  // set up first name text field
+  UITextField *firstName = [[UITextField alloc] init];
+  firstName.translatesAutoresizingMaskIntoConstraints = NO;
   firstName.textColor = [UIColor blackColor];
   firstName.font = [UIFont fontWithName:@"SFUIDisplay-Regular" size:25];
   firstName.backgroundColor = [UIColor whiteColor];
   firstName.borderStyle = UITextBorderStyleRoundedRect;
   firstName.text = @"";
   firstName.placeholder = @"First name";
+  [self.view addSubview:firstName];
+  [[[firstName topAnchor] constraintEqualToAnchor:[self.topLayoutGuide bottomAnchor] constant:10] setActive:YES];
+  [[[firstName leadingAnchor] constraintEqualToAnchor:[self.view leadingAnchor] constant:10] setActive:YES];
+  [[[firstName trailingAnchor] constraintEqualToAnchor:[self.view trailingAnchor] constant:-10] setActive:YES];
+  [[[firstName heightAnchor] constraintEqualToConstant:40] setActive:YES];
 
-  UITextField *lastName = [[UITextField alloc] initWithFrame:CGRectMake(45, firstName.frame.origin.y+75, 200, 40)];
+  // set up last name text field
+  UITextField *lastName = [[UITextField alloc] init];
+  lastName. translatesAutoresizingMaskIntoConstraints = NO;
   lastName.textColor = [UIColor blackColor];
   lastName.font = [UIFont fontWithName:@"SFUIDisplay-Regular" size:25];
   lastName.backgroundColor = [UIColor whiteColor];
   lastName.borderStyle = UITextBorderStyleRoundedRect;
   lastName.text = @"";
   lastName.placeholder = @"Last name";
-  
-  UITextField *email = [[UITextField alloc] initWithFrame:CGRectMake(45, lastName.frame.origin.y+75, 200, 40)];
+  [self.view addSubview:lastName];
+  [[[lastName topAnchor] constraintEqualToAnchor:[firstName bottomAnchor] constant:10] setActive:YES];
+  [[[lastName leadingAnchor] constraintEqualToAnchor:[self.view leadingAnchor] constant:10] setActive:YES];
+  [[[lastName trailingAnchor] constraintEqualToAnchor:[self.view trailingAnchor] constant:-10] setActive:YES];
+  [[[lastName heightAnchor] constraintEqualToConstant:40] setActive:YES];
+
+  // set up email text field
+  UITextField *email = [[UITextField alloc] init];
+  email.translatesAutoresizingMaskIntoConstraints = NO;
   email.textColor = [UIColor blackColor];
   email.font = [UIFont fontWithName:@"SFUIDisplay-Regular" size:25];
   email.backgroundColor = [UIColor whiteColor];
   email.borderStyle = UITextBorderStyleRoundedRect;
   email.text = @"";
   email.placeholder = @"Email address";
-
-  UIView *view = [[UIView alloc] initWithFrame:CGRectMake(100, 200, 400, 400)];
-  [view addSubview:firstName];
-  [view addSubview:lastName];
-  [view addSubview:email];
-
-  [self.view addSubview:view];
+  [self.view addSubview:email];
+  [[[email topAnchor] constraintEqualToAnchor:[lastName bottomAnchor] constant:10] setActive:YES];
+  [[[email leadingAnchor] constraintEqualToAnchor:[self.view leadingAnchor] constant:10] setActive:YES];
+  [[[email trailingAnchor] constraintEqualToAnchor:[self.view trailingAnchor] constant:-10] setActive:YES];
+  [[[email heightAnchor] constraintEqualToConstant:40] setActive:YES];
 }
 @end
