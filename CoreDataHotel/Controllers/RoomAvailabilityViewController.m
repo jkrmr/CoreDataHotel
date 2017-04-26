38f0c8eb9772a5384bd847e8dd3e3716bbe03f06
Jake Romer <jkrmr.io@gmail.com>
2017-04-26 12:27:00 -0700

    Refactor to use an NSFetchedResultsController

--- CoreDataHotel/Controllers/RoomAvailabilityViewController.m
+++ CoreDataHotel/Controllers/RoomAvailabilityViewController.m
@@ -9,8 +9,7 @@
 #import "RoomAvailabilityViewController.h"
 
 @interface RoomAvailabilityViewController ()
-@property (strong, nonatomic) NSArray *hotelNames;
-@property (strong, nonatomic) NSArray *rooms;
+@property (strong, nonatomic) NSFetchedResultsController *rooms;
 @property (strong, nonatomic) UITableView *tableView;
 @end
 
@@ -34,24 +33,11 @@
 }
 
 - (void) viewWillAppear:(BOOL)animated {
-  NSArray *availableRooms = [self availableRooms];
-
-  NSMutableDictionary *hotelNamesToRooms = [[NSMutableDictionary alloc] init];
-  for (Room *room in availableRooms) {
-    if (hotelNamesToRooms[room.hotel.name] == nil) {
-      hotelNamesToRooms[room.hotel.name] = [NSMutableArray array];
-    } else {
-      [hotelNamesToRooms[room.hotel.name] addObject:room];
-    }
-  }
-
-  self.hotelNames = [hotelNamesToRooms allKeys];
-  self.rooms = [hotelNamesToRooms allValues];
-  
+  self.rooms = [self availableRooms];
   [self.tableView reloadData];
 }
 
-- (NSArray*) availableRooms {
+- (NSFetchedResultsController*) availableRooms {
   AppDelegate *appDelegate = (AppDelegate*) [[UIApplication sharedApplication] delegate];
   NSManagedObjectContext *context = appDelegate.persistentContainer.viewContext;
   NSFetchRequest *req = [NSFetchRequest fetchRequestWithEntityName:@"Room"];
@@ -63,8 +49,11 @@
   [req setSortDescriptors:@[byHotelName, byRoomNumber]];
 
   NSError *fetchError;
-  NSArray *fetchedRooms = [context executeFetchRequest:req error:&fetchError];
-  
+  NSFetchedResultsController *fetchedRooms = [[NSFetchedResultsController alloc] initWithFetchRequest:req
+                                                                                 managedObjectContext:context
+                                                                                   sectionNameKeyPath:@"hotel.name"
+                                                                                            cacheName:nil];
+  [fetchedRooms performFetch:&fetchError];
   if (fetchError) {
     NSLog(@"RoomAvailabilityVC: There was a problem fetching from core data");
   }
@@ -74,7 +63,8 @@
 
 // MARK: UITableViewDataSource methods
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-  return self.hotelNames[section];
+  id<NSFetchedResultsSectionInfo> sectionInfo = [self.rooms.sections objectAtIndex:section];
+  return sectionInfo.name;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
@@ -82,17 +72,17 @@
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-  return self.hotelNames.count;
+  return self.rooms.sections.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-  NSArray* roomsForHotel = self.rooms[section];
-  return roomsForHotel.count;
+  id<NSFetchedResultsSectionInfo> sectionInfo = [self.rooms.sections objectAtIndex:section];
+  return sectionInfo.numberOfObjects;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
   UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-  Room *selectedRoom = self.rooms[indexPath.section][indexPath.row];
+  Room *selectedRoom = [self.rooms objectAtIndexPath:indexPath];
   cell.textLabel.text = [NSString stringWithFormat:@"Room %i", selectedRoom.number];
   return cell;
 }
@@ -101,7 +91,7 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
   [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
   BookViewController *bookVC = [[BookViewController alloc] init];
-  bookVC.requestedRoom = self.rooms[indexPath.section][indexPath.row];
+  bookVC.requestedRoom = [self.rooms objectAtIndexPath:indexPath];
   bookVC.requestedStartDate = self.requestedStartDate;
   bookVC.requestedEndDate = self.requestedEndDate;
   [self presentViewController:bookVC animated:YES completion:nil];
