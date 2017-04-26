127064de06014774a56efb9ba9c1c990850f6791
Jake Romer <jkrmr.io@gmail.com>
2017-04-25 23:00:40 -0700

    Cleanups

--- CoreDataHotel/Controllers/AppDelegate.m
+++ CoreDataHotel/Controllers/AppDelegate.m
@@ -30,7 +30,7 @@
   self.window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
   self.homeVC = [[HomeViewController alloc] init];
   self.navController = [[UINavigationController alloc] initWithRootViewController: self.homeVC];
-
+  
   self.window.rootViewController = self.navController;
   [self.window makeKeyAndVisible];
 }
@@ -45,7 +45,7 @@
   
   // If hotels have already been persisted, return early.
   if (count > 0) { return; }
-
+  
   NSString *path = [[NSBundle mainBundle] pathForResource: @"hotels" ofType: @"json"];
   NSData *jsonData = [NSData dataWithContentsOfFile: path];
   NSError *jsonError;
@@ -78,8 +78,6 @@
   
   if (saveError) {
     NSLog(@"Error saving to core data");
-  } else {
-    NSLog(@"Saved to core data");
   }
 }
 
@@ -101,48 +99,47 @@
 
 
 #pragma mark - Core Data stack
-
 @synthesize persistentContainer = _persistentContainer;
 
 - (NSPersistentContainer *)persistentContainer {
-    // The persistent container for the application. This implementation creates and returns a container, having loaded the store for the application to it.
-    @synchronized (self) {
-        if (_persistentContainer == nil) {
-            _persistentContainer = [[NSPersistentContainer alloc] initWithName:@"CoreDataHotel"];
-            [_persistentContainer loadPersistentStoresWithCompletionHandler:^(NSPersistentStoreDescription *storeDescription, NSError *error) {
-                if (error != nil) {
-                    // Replace this implementation with code to handle the error appropriately.
-                    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    
-                    /*
-                     Typical reasons for an error here include:
-                     * The parent directory does not exist, cannot be created, or disallows writing.
-                     * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                     * The device is out of space.
-                     * The store could not be migrated to the current model version.
-                     Check the error message to determine what the actual problem was.
-                    */
-                    NSLog(@"Unresolved error %@, %@", error, error.userInfo);
-                    abort();
-                }
-            }];
+  // The persistent container for the application. This implementation creates and returns a container, having loaded the store for the application to it.
+  @synchronized (self) {
+    if (_persistentContainer == nil) {
+      _persistentContainer = [[NSPersistentContainer alloc] initWithName:@"CoreDataHotel"];
+      [_persistentContainer loadPersistentStoresWithCompletionHandler:^(NSPersistentStoreDescription *storeDescription, NSError *error) {
+        if (error != nil) {
+          // Replace this implementation with code to handle the error appropriately.
+          // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+          
+          /*
+           Typical reasons for an error here include:
+           * The parent directory does not exist, cannot be created, or disallows writing.
+           * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+           * The device is out of space.
+           * The store could not be migrated to the current model version.
+           Check the error message to determine what the actual problem was.
+           */
+          NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+          abort();
         }
+      }];
     }
-    
-    return _persistentContainer;
+  }
+  
+  return _persistentContainer;
 }
 
 #pragma mark - Core Data Saving support
 
 - (void)saveContext {
-    NSManagedObjectContext *context = self.persistentContainer.viewContext;
-    NSError *error = nil;
-    if ([context hasChanges] && ![context save:&error]) {
-        // Replace this implementation with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        NSLog(@"Unresolved error %@, %@", error, error.userInfo);
-        abort();
-    }
+  NSManagedObjectContext *context = self.persistentContainer.viewContext;
+  NSError *error = nil;
+  if ([context hasChanges] && ![context save:&error]) {
+    // Replace this implementation with code to handle the error appropriately.
+    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+    NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+    abort();
+  }
 }
 
 @end
--- CoreDataHotel/Controllers/HomeViewController.m
+++ CoreDataHotel/Controllers/HomeViewController.m
@@ -12,7 +12,6 @@
 @end
 
 @implementation HomeViewController
-
 - (void)viewDidLoad {
   [super viewDidLoad];
   [self.view setBackgroundColor:[UIColor whiteColor]];
